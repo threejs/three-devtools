284344d9b01b3c3de4ae107d15283e8a1d2a455c
Jordan Santell <jordan@jsantell.com>
2019-05-21 16:17:14 -0700

    Add a script that copies over dependency licenses from node_modules

---
+++ scripts/genlicenses.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p web_modules/licenses
+cp node_modules/@egjs/agent/LICENSE web_modules/licenses/agent_LICENSE
+cp node_modules/three/LICENSE web_modules/licenses/three_LICENSE
+cp node_modules/lit-html/LICENSE web_modules/licenses/lit-html_LICENSE
+cp node_modules/lit-element/LICENSE web_modules/licenses/lit-element_LICENSE
