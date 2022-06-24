ae4afbd247eb03072465d85a3cb04d692c59ac45
Rafael Cordeiro <rcordeiro@cadmakers.com>
2022-06-24 10:55:36 -0700

    Update

--- src/extension/background.js
+++ src/extension/background.js
@@ -4,19 +4,19 @@ const connections = new Map();
  * When opening the three-devtools panel, store
  * a connection to communicate later.
  */
-browser.runtime.onConnect.addListener(port => {
+browser.runtime.onConnect.addListener((port) => {
   let tabId;
   const onMessage = (message) => {
     tabId = message.tabId;
-    console.log('onConnect', tabId, message.name);
-    if (message.name === 'connect') {
+    console.log("onConnect", tabId, message.name);
+    if (message.name === "connect") {
       connections.set(tabId, port);
     }
-  }
+  };
 
   port.onMessage.addListener(onMessage);
 
-  port.onDisconnect.addListener(port => {
+  port.onDisconnect.addListener((port) => {
     port.onMessage.removeListener(onMessage);
     connections.delete(tabId);
   });
@@ -40,16 +40,16 @@ browser.runtime.onMessage.addListener((request, sender) => {
  * When a page has reloaded; if three-devtools are open, notify
  * the devtools panel so it can inject the content-side of the tools.
  */
-browser.webNavigation.onCommitted.addListener(({tabId, frameId}) => {
+browser.webNavigation.onDOMContentLoaded.addListener(({ tabId, frameId }) => {
   // Only support top-level frame for now
   if (frameId !== 0) {
     return;
   }
-  console.log('onCommitted', tabId, connections.has(tabId));
+  console.log("onDOMContentLoaded", tabId, connections.has(tabId));
   if (connections.has(tabId)) {
     connections.get(tabId).postMessage({
-      type: 'committed',
-      id: 'three-devtools',
+      type: "committed",
+      id: "three-devtools",
     });
   }
 });
